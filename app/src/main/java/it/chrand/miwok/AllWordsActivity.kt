7bb8d46ac45ab309b69d7e9d63c3ba7230904f28
itchrand <ac_is_nowhere@yahoo.com>
2018-04-17 18:18:16 +0200

    Add 5 activities. change color of all word activity.

---
+++ app/src/main/java/it/chrand/miwok/AllWordsActivity.kt
@@ -0,0 +1,12 @@
+package it.chrand.miwok
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AllWordsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_words)
+    }
+}
