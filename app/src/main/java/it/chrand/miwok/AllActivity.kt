be190b66d19588d6f47d373e298e7a46a12c78c9
itchrand <ac_is_nowhere@yahoo.com>
2018-04-18 09:09:40 +0200

    Add Event Listeners for OnClick in code. change all activities labels.

--- app/src/main/java/it/chrand/miwok/AllActivity.kt
+++ app/src/main/java/it/chrand/miwok/AllActivity.kt
@@ -3,7 +3,7 @@ package it.chrand.miwok
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class AllWordsActivity : AppCompatActivity() {
+class AllActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
